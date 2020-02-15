a9c872e2030237d22a7716e4566be52148dd71bc
Bosco Basabana <basabanansita@gmail.com>
2020-02-15 19:03:55 +0200

    option category action user

---
+++ src/categories.ts
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: "Médecin",
+    icon: "/assets/icon/medecin.svg",
+    bg: "#576DB3"
+  },
+  {
+    name: "Hôpital",
+    icon: "/assets/icon/hopital.svg",
+    bg: "#C92325"
+  },
+  {
+    name: "Pharmacie",
+    icon: "/assets/icon/pharmacy.svg",
+    bg: "#051C9A"
+  },
+  {
+    name: "Pharmacie",
+    icon: "/assets/icon/pharmacy.svg",
+    bg: "#051C9A"
+  },
+  {
+    name: "Pharmacie",
+    icon: "/assets/icon/pharmacy.svg",
+    bg: "#051C9A"
+  }
+];
