c6e12aaeaefc686d90d5e458d4c7e42d87aa0373
Augustin <augumakuma@gmail.com>
2020-02-18 09:02:52 +0100

    adaptation logic home

--- src/categories.ts
+++ src/categories.ts
@@ -2,26 +2,31 @@ export default [
   {
     name: "Médecin",
     icon: "/assets/icon/medecin.svg",
-    bg: "#576DB3"
+    bg: "#576DB3",
+    url:"/list"
   },
   {
     name: "Hôpital",
     icon: "/assets/icon/hopital.svg",
-    bg: "#C92325"
+    bg: "#C92325",
+    url:"/hopital"
   },
   {
     name: "Pharmacie",
     icon: "/assets/icon/pharmacy.svg",
-    bg: "#051C9A"
+    bg: "#051C9A",
+    url:"/pharmacie"
   },
   {
     name: "Ambulance",
     icon: "/assets/icon/ambulance.svg",
-    bg: "#085084"
+    bg: "#085084",
+    url:"/ambulance"
   },
   {
     name: "B. de Sang",
     icon: "/assets/icon/blood.svg",
-    bg: "#FF0000"
+    bg: "#FF0000",
+    url:"/banquedesang"
   }
 ];
